d2adeb1fd4bb1a389dd5526357f80e82cc3718ab
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-06-30 12:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function handleSubmit(event) {
-      event.preventDefault(); // Prevent form submission
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-      const rememberMe = document.getElementById('checkbox').checked;
+// function handleSubmit(event) {
+//       event.preventDefault(); // Prevent form submission
+//       const username = document.getElementById('username').value;
+//       const password = document.getElementById('password').value;
+//       const rememberMe = document.getElementById('checkbox').checked;
       
-      if (rememberMe) {
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-      } else {
-        localStorage.removeItem('username');
-        localStorage.removeItem('password');
-      }
+//       if (rememberMe) {
+//         localStorage.setItem('username', username);
+//         localStorage.setItem('password', password);
+//       } else {
+//         localStorage.removeItem('username');
+//         localStorage.removeItem('password');
+//       }
       
-      alert('Logged in as ' + username);
-    }
+//       alert('Logged in as ' + username);
+//     }
     
     // Function to handle "Login as existing user" button click
-    function handleExistingUserLogin() {
-      const username = localStorage.getItem('username');
-      alert('Logged in as ' + username);
-    }
+    // function handleExistingUserLogin() {
+    //   const username = localStorage.getItem('username');
+    //   alert('Logged in as ' + username);
+    // }
   
     // Check if there are saved details
     // const savedUsername = localStorage.getItem('username');
